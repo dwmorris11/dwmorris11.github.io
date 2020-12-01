0cd168340e2d9de56f5220719f9327d4564f20a5
dustinmorris1 <dustinnuc@gmail.com>
2020-11-30 23:48:01 -0800

    Css for tile.  Image not rendering

--- travel/travel.js
+++ travel/travel.js
@@ -1,21 +1,48 @@
 let wishes = {};
 let key = 0;
+document.getElementById("travelDestinationForm").onsubmit = (event) => {
+    event.preventDefault();
+    submition = {
+        destination: event.target[0].value,
+        location: event.target[1].value,
+        photo: event.target[2].value,
+        description: event.target[3].value,
+    };
+    createTile(submition);
+};
+
+function displayTiles() {
+    const directions = document.getElementById("directions");
+    directions.innerHTML = "My Wish List";
+    const container = document.getElementById("wishContainer");
+    container.innerHTML = '';
+    for (let wish in wishes) {
+        container.appendChild(wishes[wish]);
+    }
+    const images = document.getElementsByTagName("image");
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+        images[i].style.display = 'block';
+    }
+}
+
 
 function createTile({
     destination,
     location,
     photo,
-    description
+    description,
 }) {
-    let uniqueKey = ++key + Math.random();
+    let uniqueKey = 'a' + ++key;
     const tile = document.createElement("div");
     const img = document.createElement("image");
     const dest = document.createElement("strong");
     const loc = document.createElement("p");
-    const desc = document.createElelent("p");
+    const desc = document.createElement("p");
     const buttonContainer = document.createElement("div");
     const editButton = document.createElement("button");
     const removeButton = document.createElement("button");
+    validateURL(photo, img);
     tile.className = "tile";
     tile.setAttribute("id", uniqueKey);
     dest.className = "tile_destination";
@@ -24,12 +51,20 @@ function createTile({
     loc.innerHTML = location;
     desc.className = "tile_description";
     desc.innerHTML = description;
-    photo = validateURL(photo);
-    img.setAttribute("src", photo);
+    buttonContainer.className = "buttonContainer";
+    img.setAttribute("width", "100%");
+    img.setAttribute("height", "100%");
+    img.setAttribute("alt", "default");
     editButton.setAttribute("data", uniqueKey);
-    editButton.onclick = (data) => edit(data);
+    editButton.setAttribute("type", "button");
+    editButton.onclick = (event) => edit(event.target.attributes.data.value);
+    editButton.innerHTML = "Edit";
+    editButton.className = "editButton";
     removeButton.setAttribute("data", uniqueKey);
-    removeButton.onclick = (data) => remove(data);
+    removeButton.onclick = (event) => remove(event.target.attributes.data.value);
+    removeButton.setAttribute("type", "button");
+    removeButton.innerHTML = "Remove";
+    removeButton.className = "removeButton";
     tile.appendChild(img);
     tile.appendChild(dest);
     tile.appendChild(loc);
@@ -38,6 +73,7 @@ function createTile({
     buttonContainer.appendChild(editButton);
     buttonContainer.appendChild(removeButton);
     wishes[uniqueKey] = tile;
+    displayTiles();
 }
 
 function edit(key) {
@@ -47,24 +83,33 @@ function edit(key) {
     const loc = element.querySelector('.tile_location');
     const desc = element.querySelector('.tile_description');
     let src = prompt("Enter photo url");
-    src = validateURL(src);
-    img.setAttribute("src", src);
+    validateURL(src, img);
     dest.innerHTML = prompt("Enter destination.");
     loc.innerHTML = prompt("Enter location.");
     desc.innerHTML = prompt("Enter description");
+    displayTiles();
 }
 
 function remove(key) {
     delete wishes[key];
+    displayTiles();
 }
 
-function validateURL(url) {
-    fetch(url)
-        .then((response) => response.json())
-        .catch((error) => {
-            url = "../resources/TulipOnly.png";
+function validateURL(url, img) {
+    if (url === "") {
+        url = 'https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg'
+    }
+    fetch(url, {
+            mode: "no-cors"
         })
-        .finally(() => {
-            return url;
-        });
+        .then(function (response) {
+            if (response.ok) {
+                img.setAttribute("src", url);
+            } else {
+                img.setAttribute("src", 'https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg');
+            }
+        })
+        .catch(() => {
+            img.setAttribute("src", 'https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg');
+        })
 }
